5f9aa66b9b504600a1600f6692ee770380d2116d
YaelMartin Alcala <ext-y.alcala@rappi.com>
2023-02-16 14:46:00 -0600

    fix: :rotating_light: fixed redirect widget

--- packages/core/lib/utils/redirect.dart
+++ packages/core/lib/utils/redirect.dart
@@ -3,11 +3,9 @@ import 'package:go_router/go_router.dart';
 import 'package:core/utils/empty_page_builder.dart';
 
 class Redirect extends GoRoute {
-  @override
-  final String path;
   final String to;
 
-  Redirect({required this.path, required this.to})
+  Redirect({required String path, required this.to})
       : super(
           path: path,
           builder: emptyPageBuilder,
